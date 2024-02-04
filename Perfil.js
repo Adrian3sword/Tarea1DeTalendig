878b199d0221a2da5298822bfeb72d89d07db7fd
Adrian3sword <adrianlorenzo353@gmail.com>
2024-02-03 20:16:32 -0400

    Se agrega el proyecto

---
+++ Perfil.js
@@ -0,0 +1,17 @@
+// function DameLosValoresDelArray(Array1) {
+//     for (let index = 0; index < Array1.length; index++) {
+//         const element = Array1[index];
+//         console.log(element);
+//     }
+// }
+
+const DameLosValoresDelArray = (Array1) => {
+    for (let index = 0; index < Array1.length; index++) {
+        const element = Array1[index];
+        console.log(element);
+    };
+};
+
+let Canasta = ["Pera","Manzana","Naranga"];
+
+DameLosValoresDelArray(Canasta);
